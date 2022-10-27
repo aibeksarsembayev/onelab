407b51ae04c29f0a66b9232611a7df6ded1b3666
quazar2000 <s-bek-k@mail.ru>
2022-10-27 16:35:07 +0600

    solved 010101hello

--- tasks/lab3/01-exercise/01-goroutines/01-hello/main.go
+++ tasks/lab3/01-exercise/01-goroutines/01-hello/main.go
@@ -19,12 +19,21 @@ func main() {
 	// TODO: write goroutine with different variants for function call.
 
 	// goroutine function call
-
+	go fun("go function call")
 	// goroutine with anonymous function
-
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Println("go anonymous function")
+		}
+	}()
 	// goroutine with function value call
-
+	go func(limiter int) {
+		for i := 0; i < limiter; i++ {
+			fmt.Println("go anonymous function with value")
+		}
+	}(3)
 	// wait for goroutines to end
-
+	time.Sleep(time.Second)
+	
 	fmt.Println("done..")
 }
