72964e80ae89a9686d759c8596b42ba9f3a7b57e
quazar2000 <s-bek-k@mail.ru>
2022-10-27 19:37:50 +0600

    010202channel

--- tasks/lab3/01-exercise/02-channel/02-channel/main.go
+++ tasks/lab3/01-exercise/02-channel/02-channel/main.go
@@ -1,12 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	ch := make(chan int)
+
 	go func() {
 		for i := 0; i < 6; i++ {
 			// TODO: send iterator over channel
+			ch <- i
 		}
 	}()
 
 	// TODO: range over channel to recv values
+
+	for i := 0; i < 6; i++ {
+		fmt.Println(<-ch)
+	}
 
 }
