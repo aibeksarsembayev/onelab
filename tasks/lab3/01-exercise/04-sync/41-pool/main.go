c54a0f5f08cbf806d2bcd2806384b1746a4c7497
quazar2000 <s-bek-k@mail.ru>
2022-10-27 23:41:00 +0600

    010441pool

--- tasks/lab3/01-exercise/04-sync/41-pool/main.go
+++ tasks/lab3/01-exercise/04-sync/41-pool/main.go
@@ -4,13 +4,24 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"sync"
 	"time"
 )
 
 //TODO: create pool of bytes.Buffers which can be reused.
+var bufPool = sync.Pool{
+	New: func() any {
+		// The Pool's New function should generally only return pointer
+		// types, since a pointer can be put into the return interface
+		// value without an allocation:
+		return new(bytes.Buffer)
+	},
+}
 
 func log(w io.Writer, val string) {
-	var b bytes.Buffer
+	// var b bytes.Buffer
+	b := bufPool.Get().(*bytes.Buffer)
+	b.Reset()
 
 	b.WriteString(time.Now().Format("15:04:05"))
 	b.WriteString(" : ")
@@ -18,6 +29,8 @@ func log(w io.Writer, val string) {
 	b.WriteString("\n")
 
 	w.Write(b.Bytes())
+
+	bufPool.Put(b)
 }
 
 func main() {
