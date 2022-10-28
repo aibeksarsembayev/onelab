34fe45dfb44b0dcb0a4b960d8b53ebd1ad671b12
quazar <s-bek-k@mail.ru>
2022-10-28 17:19:10 +0600

    020303timeout

--- tasks/lab3/02-exercise/03-context/03-timeout/main.go
+++ tasks/lab3/02-exercise/03-context/03-timeout/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 
 	// TODO: set a http client timeout
+	timeout := 500 * time.Millisecond
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 
 	req, err := http.NewRequest("GET", "https://andcloud.io", nil)
 	if err != nil {
@@ -23,8 +28,13 @@ func main() {
 	}
 
 	// Close the response body on the return.
-	defer resp.Body.Close()
+	// defer resp.Body.Close()
+	select {
+	case <-ctx.Done():
+		resp.Body.Close()
+	default:
+		// Write the response to stdout.
+		io.Copy(os.Stdout, resp.Body)
+	}
 
-	// Write the response to stdout.
-	io.Copy(os.Stdout, resp.Body)
 }
