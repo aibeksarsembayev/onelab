d2dd6602d070839344d8441b76302db5cd5e5ef5
quazar <s-bek-k@mail.ru>
2022-10-28 14:36:41 +0600

    0105race

--- tasks/lab3/01-exercise/05-race/main.go
+++ tasks/lab3/01-exercise/05-race/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -12,10 +13,17 @@ import (
 func main() {
 	start := time.Now()
 	var t *time.Timer
+	mu := sync.Mutex{}
+	mu.Lock()
 	t = time.AfterFunc(randomDuration(), func() {
+
 		fmt.Println(time.Now().Sub(start))
+		mu.Lock()
 		t.Reset(randomDuration())
+		mu.Unlock()
 	})
+	mu.Unlock()
+
 	time.Sleep(5 * time.Second)
 }
 
